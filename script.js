a6bf05694c3b65f0bfe911df5ce6ebcc7b257701
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-04-07 12:49:20 +0100

    initial setup

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert("welcome")
+const options = {
+    Authorization: "Bearer 563492ad6f91700001000001c24c1792b0394844aefc6c1957db294b"
+}
+
+fetch("https://api.pexels.com/v1/search?query=cat", options)
+.then(response => response.json()
+.then(body => { 
+    console.log(body)
+    
+    console.log(body.photos[0].alt)
+
+
+
+
+}))
