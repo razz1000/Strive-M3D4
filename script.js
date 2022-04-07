47d699a4778efd130d85e6d99cedc6078e104a85
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-04-07 13:21:00 +0100

    cards showing

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 const options = {
-    Authorization: "Bearer 563492ad6f91700001000001c24c1792b0394844aefc6c1957db294b"
+
 }
 
-fetch("https://api.pexels.com/v1/search?query=cat", options)
+fetch("https://striveschool-api.herokuapp.com/books", options)
 .then(response => response.json()
-.then(body => { 
-    console.log(body)
-    
-    console.log(body.photos[0].alt)
+.then(books => { 
+    console.log(books)
+    console.log(books[0].img)
 
+    const row1 = document.querySelector(".row")
 
+    books.forEach(book => {
+    const col1 = document.createElement("div");
+    col1.classList.add("col-sm-4")
 
+    col1.innerHTML = `
+    <div class="card" style="width: 18rem;">
+        <img src="${book.img}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Add to cart</a>
+                <a href="#" class="btn btn-primary">Skip</a>
+            </div>
+    </div>
+    `
+    row1.appendChild(col1)        
+    });
 
 }))
