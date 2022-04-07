aac085632591fea5633a532ba102b755de50d8ae
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-04-07 15:50:00 +0100

    All exercises done (missing the extras)

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ fetch("https://striveschool-api.herokuapp.com/books", options)
 .then(response => response.json()
 .then(books => { 
     console.log(books)
-    console.log(books[0].img)
 
     booksArr = [...books];
 
@@ -33,24 +32,49 @@ fetch("https://striveschool-api.herokuapp.com/books", options)
 }))
 
 
-/* This is the addToCartFunction */
+/* This is the "click" counter */
+let clicks = 0;
+
+/* THIS IS THE ADDTOCART FUNCTION */
 let addToCartClickFunction = (event) => {
-    console.log(event.target.closest("div").innerHTML)
+     console.log(event.target.closest(".card").querySelector("h5").innerText) 
+
+    /* The cartClick counter */
+    clicks += 1;
+    document.querySelector(".cart-number").innerHTML = clicks;
+ 
 
     card1 = event.target.parentElement.parentElement;
+    
+    /* The change of styling */
+    card1.classList.toggle("clicked-card")
+
     const row2 = document.querySelector(".row2")
+
+    const sourceImg = event.target.closest(".card").querySelector("img");
+    const bookTitle = event.target.closest(".card").querySelector("h5").innerText;
 
     row2.innerHTML += `
     <div class="card">
-        <img src="${event.target.closest("div")}" class="card-img-top" alt="...">
+        <img src="${sourceImg.src}" class="card-img-top" alt="..." style="width: 50px;">
             <div class="card-body">
-                <h5 class="card-title">${event.target.parentElement.innerText}</h5>
-                <a href="#" class="btn btn-dark" >remove</a>
+                <h5 class="card-title">${bookTitle}</h5>
+                <a href="#" class="btn btn-dark" onclick="removeFromCartFunction(event)">remove</a>
                 
             </div>
     </div>
     `
 }
+
+    /* This is the removeFromCartFunction */
+    let removeFromCartFunction = (event) => {
+        console.log(event.target.parentElement.parentElement)
+        event.target.parentElement.parentElement.remove();
+        
+        clicks -= 1;
+        document.querySelector(".cart-number").innerHTML = clicks;
+
+    }
 
     /* This is the Skip/HideButton function */
     let hideButton = (event) => {
@@ -87,6 +111,4 @@ let addToCartClickFunction = (event) => {
             `
             row1.appendChild(col3)
         })
-
-
     }
